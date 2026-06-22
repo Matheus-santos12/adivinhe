4500f8830ad6bf2ad5bb1ffbf92f0273b2ed2a98
Matheus Silva <rematheussilva@gmail.com>
2026-06-22 16:49:56 -0500

    feat: eneble dynamic header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import styles from "./app.module.css";
 import { Header } from "./components/Header";
 
 export default function App() {
+  function handleRestartGame() {
+    alert("Reiniciar jogo");
+  }
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={10} onRestart={handleRestartGame} />
       </main>
     </div>
   );

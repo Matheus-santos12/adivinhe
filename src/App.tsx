6cd7d60b9b98f8da67083e932306eb5e5c3d55ba
Matheus Silva <rematheussilva@gmail.com>
2026-06-24 15:06:02 -0500

    feat: adds valid letter check functionality

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,29 @@ export default function App() {
     setLetter("");
   }
 
+  function handleConfirm() {
+    if (!challenge) {
+      return;
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra!");
+    }
+
+    const value = letter.toUpperCase();
+    const exists = lettersUsed.find(
+      (used) => used.value.toUpperCase() === value,
+    );
+
+    if (exists) {
+      return alert("Você já utilizou essa letra!. Tente outra" + value);
+    }
+
+    setLettersUsed((prevState) => [...prevState, { value, correct: false }]);
+
+    setLetter("");
+  }
+
   useEffect(() => {
     startGame();
   }, []);
@@ -52,8 +75,14 @@ export default function App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar" />
+          <Input
+            autoFocus
+            maxLength={1}
+            placeholder="?"
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+          />
+          <Button title="Confirmar" onClick={handleConfirm} />
         </div>
 
         <LettersUsed data={lettersUsed} />
